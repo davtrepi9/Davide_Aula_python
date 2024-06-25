e4ac308ee295b24bdd09145fcb1bde24a9206409
Davide Pappalardo <davidepappalardoinf@outlook.com>
2024-06-25 12:30:19 +0200

    Add files via upload

--- Settimana2/martedi/esercitazione6.py
+++ Settimana2/martedi/esercitazione6.py
@@ -11,6 +11,16 @@ def palindromo(inp):
     inversa=inp[::-1]
     return True if inp == inversa else False
 
+def palindromo2(inp):
+    inversa=list(reversed(inp))
+    return True if list(inp) == inversa else False
+
+def palindromo3(inp):
+    inversa="".join(reversed(inp))
+    return True if inp == inversa else False
 
 s=input("Inserisci stringa: ")
-print(palindromo(s.lower()))
+
+print("1: Palindromo: ",palindromo(s.lower()))
+print("2: Palindromo: ",palindromo2(s.lower()))
+print("3: Palindromo: ",palindromo3(s.lower()))
