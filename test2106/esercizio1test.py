e10e402d0f6ba5896edf03000553e019c01826d3
Davide Pappalardo <davidepappalardoinf@outlook.com>
2024-07-01 15:37:46 +0200

    Delete test2106 directory

--- test2106/esercizio1test.py
+++
@@ -1,7 +0,0 @@
-contatore=0
-
-inp=input("Inserisci una stringa: ")
-for i in inp:
- if (i=="a" or i=="e" or i=="i" or i =="o" or i=="u"):
-     contatore=contatore+1
-print("Numero vocali trovate: ",contatore)
