90c39a40307cefad903aa5f70773ae1d9a16ba3b
Davide Pappalardo <davidepappalardoinf@outlook.com>
2024-07-16 10:18:01 +0200

    aggiunto menu

--- numpylib/numpyese_2.py
+++ numpylib/numpyese_2.py
@@ -12,24 +12,80 @@ come l'inversione delle righe e la sostituzione condizionale degli elementi."""
 
 import numpy as np
 
-matrice = np.random.randint(1, 101, size=(6,6))
-print("\nMatrice 6x6 generata : ")
-print(matrice)
+def genera_matrice():
+    matrice = np.random.randint(1, 101, size=(6,6))
+    print("\nMatrice 6x6 generata : ")
+    print(matrice)
+    return matrice
 
-centrale = matrice[1:5,1:5]
-print("\nSotto-matrice centrale : ")
-print(centrale)
+def centrale(matrice):
+    centrale = matrice[1:5,1:5]
+    print("\nSotto-matrice centrale : ")
+    print(centrale)
+    return centrale
 
-inversa = matrice[::-1]
-print("\nMatrice con righe invertite : ")
-print(inversa)
+def inverti(matrice):
+    inversa = matrice[::-1]
+    print("\nMatrice con righe invertite : ")
+    print(inversa)
+    return inversa
 
-diagonale = np.diag(matrice)
-print("\nDiagonale della matrice : ")
-print(diagonale)
+def diagonal(matrice):
+    diagonale = np.diag(matrice)
+    print("\nDiagonale della matrice : ")
+    print(diagonale)
 
-inversa[inversa % 3 == 0] = -1
-print("\nInversa con multipli di 3 cambiati a -1 : ")
-print(inversa)
+def multipli(inversa):
+    if inversa is not None:
+        inversa[inversa % 3 == 0] = -1
+        print("\nInversa con multipli di 3 cambiati a -1 : ")
+        print(inversa)
+        return inversa
+    else: print("Matrice assente")
 
 
+
+#MAIN
+
+matrice_originale = None
+matrice_centrale = None
+matrice_invertita = None
+
+while True:
+        print("\nMenu:")
+        print("1. Genera matrice 6x6")
+        print("2. Estrai sotto-matrice centrale 4x4")
+        print("3. Inverti righe della sotto-matrice")
+        print("4. Estrai diagonale della matrice invertita")
+        print("5. Sostituisci multipli di 3 con -1 nella matrice invertita")
+        print("6. Esci")
+
+        scelta = input("Scegli un'opzione: ")
+
+        if scelta == '1':
+            matrice_originale = genera_matrice()
+        elif scelta == '2':
+            if matrice_originale is not None:
+                matrice_centrale = centrale(matrice_originale)
+            else:
+                print("Genera prima la matrice originale!")
+        elif scelta == '3':
+            if matrice_centrale is not None:
+                matrice_invertita = inverti(matrice_centrale)
+            else:
+                print("Estrai prima la sotto-matrice centrale!")
+        elif scelta == '4':
+            if matrice_invertita is not None:
+                diagonal(matrice_invertita)
+            else:
+                print("Inverti prima le righe della sotto-matrice!")
+        elif scelta == '5':
+            if matrice_invertita is not None:
+                multipli(matrice_invertita)
+            else:
+                print("Inverti prima le righe della sotto-matrice!")
+        elif scelta == '6':
+            print("Uscita dal programma.")
+            break
+        else:
+            print("Scelta non valida. Riprova.")
